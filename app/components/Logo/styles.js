05e8ebb874aeaecda2382f24d933d0c9b5395ec9
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2018-06-28 11:58:28 +0530

    Home Page And Logo Added

---
+++ app/components/Logo/styles.js
@@ -0,0 +1,28 @@
+import EStyleSheet from 'react-native-extended-stylesheet';
+import { Dimensions } from 'react-native';
+
+
+const imageWidth = Dimensions.get('window').width/2;
+
+export default EStyleSheet.create({
+    container:{
+        alignItems: 'center'
+    },
+    containerImage:{
+        alignItems: 'center',
+        justifyContent: 'center',
+        width : imageWidth,
+        height : imageWidth,
+    },
+    image:{
+        width: imageWidth/2,
+        height: imageWidth/2,
+    },
+    text:{
+        color: 'white',
+        fontWeight: '600',
+        fontSize: 28,
+        letterSpacing: -0.5,
+        marginTop: 15,
+    }
+});
